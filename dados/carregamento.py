7c3ee6f771858bd162fd2bd0ef2fe8674fb07e40
Aline <alinelimadonascimenton@gmail.com>
2026-06-03 09:41:07 -0300

    update funcao de carregamento

--- dados/carregamento.py
+++ dados/carregamento.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from configs.paths import ARQUIVO_CSV, ESTATISTICAS_CLASSIFICACOES
+from configs.paths import ARQUIVO_CSV
 
 def carregar_dados():
     df_colunas = pd.read_csv(ARQUIVO_CSV, nrows=0, sep=None, engine="python", dtype={"Classificação": str})
@@ -7,12 +7,14 @@ def carregar_dados():
                  if len(str(coluna)) == 7 and str(coluna)[4] == "-"]
 
     emprestimos = pd.read_csv(ARQUIVO_CSV, sep=None, engine="python")
+    emprestimos[COLUNAS_MESES] = emprestimos[COLUNAS_MESES].apply(pd.to_numeric, errors="coerce").fillna(0)
+
     acervos = emprestimos.groupby(
         ["Classificação", "Código do acervo", "Título"],
         as_index=False
     )[COLUNAS_MESES].sum()
 
-    estatistica_descritiva = pd.read_csv(ESTATISTICAS_CLASSIFICACOES, sep=None, engine="python")
-    df = pd.merge(acervos, estatistica_descritiva, how='left', on='Classificação')
+    return acervos, COLUNAS_MESES
 
-    return df, COLUNAS_MESES
+def juntar_acervos_com_estatisticas(acervos, estatisticas):
+    return pd.merge(acervos, estatisticas, how='left', on='Classificação')

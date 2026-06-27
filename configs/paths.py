756309c6dc1681db9acac54266a09131e5d11063
Aline Lima <125333369+alinelimx@users.noreply.github.com>
2026-06-27 18:15:20 -0300

    Fix string quotes in paths.py

--- configs/paths.py
+++ configs/paths.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 
-ARQUIVO_CSV = 'MatrizHierarquicaTemporal.csv'
+ARQUIVO_CSV = "MatrizHierarquicaTemporal.csv"
 
 PASTA_EDA = Path("EDA")
 PASTA_GRAFICOS = PASTA_EDA / "graficos"
 
-ESTATISTICAS_CLASSIFICACOES = (
-    PASTA_EDA / "estatistica_descritiva_por_classificacao.csv"
-)
+ESTATISTICAS_CLASSIFICACOES = PASTA_EDA / "estatistica_descritiva_por_classificacao.csv"
